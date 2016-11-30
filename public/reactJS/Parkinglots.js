37f578cdfbee1892bd43fc8dde306d26ce9739bf
John Larsson <john.larsson@hiq.se>
2016-11-30 19:21:06 +0100

    Added fix för mad display

--- public/reactJS/Parkinglots.js
+++ public/reactJS/Parkinglots.js
@@ -5,10 +5,13 @@ import React from 'react';
 
 class ParkingLot extends React.Component {
     render(){
-        let mapUrl = 'https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=640x640&scale=2&markers=color:blue%7Clabel:S|' + this.props.lot.location + '&key=AIzaSyD55li1OuTm-bRAzfO4Mo3AsdNKHywfp1s'
+        let mapUrl = "https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=640x640&scale=2&markers=color:blue%7Clabel:S|" + encodeURIComponent(this.props.lot.location) + "&key=AIzaSyD55li1OuTm-bRAzfO4Mo3AsdNKHywfp1s";
+        console.log(mapUrl);
         return(
-            <section onClick={()=>{this.props.onClick(this.props.lot)}} className="col-md-6 col-sm-6 col-xs-12" style={{backgroundImage:url(mapUrl)}}>
-                <span className="lot-name">{this.props.lot.name}</span>
+            <section style={{backgroundImage:'url(' + mapUrl + ')'}} onClick={()=>{this.props.onClick(this.props.lot)}} className={"col-md-6 col-sm-6 col-xs-12 lot"} >
+                <span className="lot-name">
+                    <h1>{this.props.lot.name}</h1>
+                </span>
             </section>
         );
     }
@@ -21,10 +24,11 @@ export default class Parkinglots extends React.Component{
     render(){
         let lots = [];
         this.props.lots.forEach((lot) => {
-            lots.push(<ParkingLot lot={lot}/>)
+            lots.push(<ParkingLot lot={lot} key={lot.id + lot.name}/>)
         });
         return(
-            <div className="row no-gutter">
+            <div className="row no-gutter lots">
+                {lots}
                 {lots}
             </div>
         );
